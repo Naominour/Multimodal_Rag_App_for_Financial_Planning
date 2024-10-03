c8af3e6f43d9f19082a801c89d61699cb9305292
Naomi <naeimehnour@gmail.com>
2024-10-03 09:58:55 +0100

    Libraries Imported

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 #Import dependencies
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.chain import LLMChain
+from langchain_community.prompts import PromptTemplate
+from langchain_community.shema.document import Document
+from langchain.schema_messages import HummanMessage, SystemMessage
+from langchain.vectorstores import FAISS
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from PIL import Image
+import os
+import nltk
+import base64
+import uuid
+
